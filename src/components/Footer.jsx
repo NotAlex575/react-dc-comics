9e23653446afdac1ccc21c4644220ddfc4543bb9
NotAlex575 <dapontealessandro89gmail.com>
2025-08-25 16:06:55 +0200

    spostati assets in public, aggiunto footer con contenuto inizale

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,36 @@
 const Footer = () => {
   return (
-    <div>Questo e il footer!</div>
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="iscriviti">
+            <button>Sign up now</button>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <a href="#"><p className="Follow-us">FOLLOW US</p></a>
+              </li>
+              <li>
+                <a href="#"><img src="/footer-facebook.png" alt="" /></a>
+              </li>
+              <li>
+                <a href="#"><img src="/footer-twitter.png" alt="" /></a>
+              </li>
+              <li>
+                <a href="#"><img src="/footer-youtube.png" alt="" /></a>
+              </li>
+              <li>
+                <a href="#"><img src="/footer-pinterest.png" alt="" /></a>
+              </li>
+              <li>
+                <a href="#"><img src="/footer-periscope.png" alt="" /></a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </footer>
   )
 }
 
