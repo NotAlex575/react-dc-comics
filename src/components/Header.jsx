c46ecf04b46e0d4cd59dbc4b64e6fb74a396477c
NotAlex575 <dapontealessandro89gmail.com>
2025-08-25 15:57:35 +0200

    creata navbar header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,40 @@ const Header = () => {
             <div className="logo">
               <img src="/dc-logo.png" alt="Logo" />
             </div>
-            <p>navbar</p>
+            <nav>
+              <ul>
+                <li>
+                  <a href="#">Characters</a>
+                </li>
+                <li>
+                  <a href="#">Comics</a>
+                </li>
+                <li>
+                  <a href="#">Movies</a>
+                </li>
+                <li>
+                  <a href="#">Tv</a>
+                </li>
+                <li>
+                  <a href="#">Games</a>
+                </li>
+                <li>
+                  <a href="#">Collectibles</a>
+                </li>
+                <li>
+                  <a href="#">Videos</a>
+                </li>
+                <li>
+                  <a href="#">Fans</a>
+                </li>
+                <li>
+                  <a href="#">News</a>
+                </li>
+                <li>
+                  <a href="#">Shop</a>
+                </li>
+              </ul>
+            </nav>
           </div>
         </div>
     </header>
