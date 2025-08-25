82ae7107507d3e9ef296327febea933f86289cda
NotAlex575 <dapontealessandro89gmail.com>
2025-08-25 18:01:51 +0200

    ultimati punti del footer grande

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,81 @@
 const Footer = () => {
   return (
     <footer>
-      <div className="container">
-        <div className="row h-80per bg-image">
-          <div className="w-50per"></div>
-          <div className="w-50per"></div>
-        </div>
-        <div className="row margin-row-footer h-20per bg-gray">
-          <div className="iscriviti">
-            <button>SIGN-UP NOW!</button>
+      <div className="bg-image">
+        <div className="container">
+          <div className="row h-80per">
+            <div className="row-footer-info w-60per">
+              <div className="col">
+                <h2 className="color-white">DC COMICS</h2>
+                <a className="color-gray" href="#">Characters</a>
+                <a className="color-gray" href="#">Comics</a>
+                <a className="color-gray" href="#">Movies</a>
+                <a className="color-gray" href="#">Tv</a>
+                <a className="color-gray" href="#">Games</a>
+                <a className="color-gray" href="#">Videos</a>
+                <a className="color-gray" href="#">News</a>
+                <h2 className="color-white">SHOP</h2>
+                <a className="color-gray" href="#">Shop DC</a>
+                <a className="color-gray" href="#">Shop DC Collectibles</a>
+              </div>
+              <div className="col">
+                <h2 className="color-white">DC</h2>
+                <a className="color-gray" href="#">Term of use</a>
+                <a className="color-gray" href="#">Privacy policy (New)</a>
+                <a className="color-gray" href="#">Ad Choices</a>
+                <a className="color-gray" href="#">Advertising</a>
+                <a className="color-gray" href="#">Jobs</a>
+                <a className="color-gray" href="#">Subscriptions</a>
+                <a className="color-gray" href="#">Talent Workshops</a>
+                <a className="color-gray" href="#">CPSC Certificate</a>
+                <a className="color-gray" href="#">Ratings</a>
+                <a className="color-gray" href="#">Shop help</a>
+                <a className="color-gray" href="#">Contact us</a>
+              </div>
+              <div className="col">
+                <h2 className="color-white">SITES</h2>
+                <a className="color-gray" href="#">DC</a>
+                <a className="color-gray" href="#">MAD Magazine</a>
+                <a className="color-gray" href="#">DC Kids</a>
+                <a className="color-gray" href="#">DC Universe</a>
+                <a className="color-gray" href="#">DC Power Visa</a>
+              </div>
+            </div>
+            <div className="w-40per logo-bg flex justify-content-center align-items-center">
+              <img className="overflow-hidden" src="/dc-logo-bg.png" alt="" />
+            </div>
           </div>
-          <nav>
-            <ul>
-              <li>
-                <a href="#"><p className="Follow-us">FOLLOW US</p></a>
-              </li>
-              <li>
-                <a href="#"><img src="/footer-facebook.png" alt="" /></a>
-              </li>
-              <li>
-                <a href="#"><img src="/footer-twitter.png" alt="" /></a>
-              </li>
-              <li>
-                <a href="#"><img src="/footer-youtube.png" alt="" /></a>
-              </li>
-              <li>
-                <a href="#"><img src="/footer-pinterest.png" alt="" /></a>
-              </li>
-              <li>
-                <a href="#"><img src="/footer-periscope.png" alt="" /></a>
-              </li>
-            </ul>
-          </nav>
+        </div>
+      </div>
+      <div className="bg-gray">
+        <div className="container">
+          <div className="row margin-row-footer h-64px">
+            <div className="iscriviti">
+              <button>SIGN-UP NOW!</button>
+            </div>
+            <nav>
+              <ul>
+                <li>
+                  <a href="#"><p className="Follow-us">FOLLOW US</p></a>
+                </li>
+                <li>
+                  <a href="#"><img src="/footer-facebook.png" alt="" /></a>
+                </li>
+                <li>
+                  <a href="#"><img src="/footer-twitter.png" alt="" /></a>
+                </li>
+                <li>
+                  <a href="#"><img src="/footer-youtube.png" alt="" /></a>
+                </li>
+                <li>
+                  <a href="#"><img src="/footer-pinterest.png" alt="" /></a>
+                </li>
+                <li>
+                  <a href="#"><img src="/footer-periscope.png" alt="" /></a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
     </footer>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,35 +1,41 @@
 const Main = () => {
   return (
     <main>
-      <div className="container">
-        <div className="bg-black h-50per flex align-items-center">
-          <div className="color-white">
-            <p className="text-center">Content goes here!</p>
+      <div className="bg-black w-100per h-50per">
+        <div className="container">
+          <div className="bg-black flex align-items-center">
+            <div className="color-white margin-top-70">
+              <p className="text-center">Content goes here!</p>
+            </div>
           </div>
         </div>
-        <div className="row bg-blue h-50per">
-            <a className="row" href="#">
-              <img src="/buy-comics-digital-comics.png" alt="" />
-              <p className="color-white">DIGITAL COMICS</p>
-            </a>
-            <a className="row" href="#">
-              <img src="/buy-comics-merchandise.png" alt="" />
-              <p className="color-white">DC MERCHANDISE</p>
-            </a>
-            <a className="row" href="#">
-              <img src="/buy-comics-subscriptions.png" alt="" />
-              <p className="color-white">SUBSCRIPTION</p>
-            </a>
-            <a className="row" href="#">
-              <img src="/buy-comics-shop-locator.png" alt="" />
-              <p className="color-white">COMIC SHOP LOCATOR</p>
-            </a>
-            <a className="row" href="#">
-              <img src="/buy-dc-power-visa.svg" alt="" />
-              <p className="color-white">DC POWER VISA</p>
-            </a>
-        </div>
       </div>
+      <div className="bg-blue w-100per h-50per">
+        <div className="container">
+            <div className="row h-50per">
+                <a className="row margin-top-70" href="#">
+                  <img src="/buy-comics-digital-comics.png" alt="" />
+                  <p className="color-white">DIGITAL COMICS</p>
+                </a>
+                <a className="row margin-top-70" href="#">
+                  <img src="/buy-comics-merchandise.png" alt="" />
+                  <p className="color-white">DC MERCHANDISE</p>
+                </a>
+                <a className="row margin-top-70" href="#">
+                  <img src="/buy-comics-subscriptions.png" alt="" />
+                  <p className="color-white">SUBSCRIPTION</p>
+                </a>
+                <a className="row margin-top-70" href="#">
+                  <img src="/buy-comics-shop-locator.png" alt="" />
+                  <p className="color-white">COMIC SHOP LOCATOR</p>
+                </a>
+                <a className="row margin-top-70" href="#">
+                  <img src="/buy-dc-power-visa.svg" alt="" />
+                  <p className="color-white">DC POWER VISA</p>
+                </a>
+            </div>
+          </div>
+        </div>
     </main>
   )
 }
