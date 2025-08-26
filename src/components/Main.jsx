6d27659ca1563ded3ace267e95829982d65705da
NotAlex575 <dapontealessandro89gmail.com>
2025-08-26 18:49:21 +0200

    inseriti esempi risultati del container -> row -> col

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,4 @@
+import Comics from "../../scripts/comics";
 
 
 const Main = () => {
@@ -5,7 +6,7 @@ const Main = () => {
     <main>
       <div className="bg-img-main w-100per h-40per">
         <div className="container">
-          <div className="padding-top-300px">
+          <div className="padding-top-current-series">
             <div className="current-series">
               <p className="h-100per flex justify-content-center align-items-center">CURRENT SERIES</p>
             </div>
@@ -13,10 +14,67 @@ const Main = () => {
         </div>
       </div>
       <div className="bg-black w-100per h-40per">
-        <div className="container">
-          <div className="bg-black flex align-items-center">
-            <div className="color-white margin-top-70">
-              <p className="text-center">Content goes here!</p>
+        <div className="container padding-top-40">
+          <div className="row wrap">
+            <div className="col-card prod-card">
+              <div className="col justify-content-center align-items-center w-100per">
+                <img src="/buy-comics-digital-comics.png" alt="" />
+                <p>biufdfsbdifub</p>
+              </div>
+            </div>
+            <div className="col-card prod-card">
+              <div className="col justify-content-center align-items-center w-100per">
+                <img src="/buy-comics-digital-comics.png" alt="" />
+                <p>biufdfsbdifub</p>
+              </div>
+            </div>
+           <div className="col-card prod-card">
+              <div className="col justify-content-center align-items-center w-100per">
+                <img src="/buy-comics-digital-comics.png" alt="" />
+                <p>biufdfsbdifub</p>
+              </div>
+            </div>
+            <div className="col-card prod-card">
+              <div className="col justify-content-center align-items-center w-100per">
+                <img src="/buy-comics-digital-comics.png" alt="" />
+                <p>biufdfsbdifub</p>
+              </div>
+            </div>
+             <div className="col-card prod-card">
+              <div className="col justify-content-center align-items-center w-100per">
+                <img src="/buy-comics-digital-comics.png" alt="" />
+                <p>biufdfsbdifub</p>
+              </div>
+            </div>
+             <div className="col-card prod-card">
+              <div className="col justify-content-center align-items-center w-100per">
+                <img src="/buy-comics-digital-comics.png" alt="" />
+                <p>biufdfsbdifub</p>
+              </div>
+            </div>
+            <div className="col-card prod-card">
+              <div className="col justify-content-center align-items-center w-100per">
+                <img src="/buy-comics-digital-comics.png" alt="" />
+                <p>biufdfsbdifub</p>
+              </div>
+            </div>
+            <div className="col-card prod-card">
+              <div className="col justify-content-center align-items-center w-100per">
+                <img src="/buy-comics-digital-comics.png" alt="" />
+                <p>biufdfsbdifub</p>
+              </div>
+            </div>
+            <div className="col-card prod-card">
+              <div className="col justify-content-center align-items-center w-100per">
+                <img src="/buy-comics-digital-comics.png" alt="" />
+                <p>biufdfsbdifub</p>
+              </div>
+            </div>
+            <div className="col-card prod-card">
+              <div className="col justify-content-center align-items-center w-100per">
+                <img src="/buy-comics-digital-comics.png" alt="" />
+                <p>biufdfsbdifub</p>
+              </div>
             </div>
           </div>
         </div>
@@ -25,23 +83,23 @@ const Main = () => {
         <div className="container">
             <div className="row h-50per">
                 <a className="row margin-top-70" href="#">
-                  <img src="/buy-comics-digital-comics.png" alt="" />
+                  <img className="logo" src="/buy-comics-digital-comics.png" alt="" />
                   <p className="color-white">DIGITAL COMICS</p>
                 </a>
                 <a className="row margin-top-70" href="#">
-                  <img src="/buy-comics-merchandise.png" alt="" />
+                  <img className="logo" src="/buy-comics-merchandise.png" alt="" />
                   <p className="color-white">DC MERCHANDISE</p>
                 </a>
                 <a className="row margin-top-70" href="#">
-                  <img src="/buy-comics-subscriptions.png" alt="" />
+                  <img className="logo" src="/buy-comics-subscriptions.png" alt="" />
                   <p className="color-white">SUBSCRIPTION</p>
                 </a>
                 <a className="row margin-top-70" href="#">
-                  <img src="/buy-comics-shop-locator.png" alt="" />
+                  <img className="logo" src="/buy-comics-shop-locator.png" alt="" />
                   <p className="color-white">COMIC SHOP LOCATOR</p>
                 </a>
                 <a className="row margin-top-70" href="#">
-                  <img src="/buy-dc-power-visa.svg" alt="" />
+                  <img className="logo" src="/buy-dc-power-visa.svg" alt="" />
                   <p className="color-white">DC POWER VISA</p>
                 </a>
             </div>
